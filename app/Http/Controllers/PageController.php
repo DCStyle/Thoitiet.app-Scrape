b7bc7c84814400aaadded2e96083e6fc9b889f02
dcstylexf <dcstylexf@gmail.com>
2025-02-25 19:25:35 +0700

    Update

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use App\Models\Page;
+use RalphJSmit\Laravel\SEO\Support\SEOData;
 
 class PageController extends Controller
 {
@@ -13,6 +14,16 @@ class PageController extends Controller
             ->take(5)
             ->get();
 
-        return view('pages.show', compact('page', 'latestArticles'));
+        $customMetadata = $this->getMetadata();
+        $SEOData = new SEOData(
+            $customMetadata['title'] ?? null,
+            $customMetadata['description'] ?? null
+        );
+
+        return view('pages.show', compact(
+            'page',
+            'latestArticles',
+            'SEOData'
+        ));
     }
 }
