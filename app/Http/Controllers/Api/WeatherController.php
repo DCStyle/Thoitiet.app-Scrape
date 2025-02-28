e9bd4f977da78fdb2f52c90c33cd8f15737e82da
dcstylexf <dcstylexf@gmail.com>
2025-02-28 14:46:02 +0700

    Update

--- app/Http/Controllers/Api/WeatherController.php
+++ app/Http/Controllers/Api/WeatherController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class WeatherController extends Controller
