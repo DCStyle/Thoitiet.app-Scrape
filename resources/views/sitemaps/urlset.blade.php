ecfe78ac3f0d21dc82131f22618469dd2dec6099
dcstylexf <dcstylexf@gmail.com>
2025-03-18 16:07:55 +0700

    Update

--- resources/views/sitemaps/urlset.blade.php
+++ resources/views/sitemaps/urlset.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
     @foreach($urls as $url)
         <url>
-            <loc>{{ str_replace(config('url_mappings.source_domain'), env('APP_URL'), $url->url) }}</loc>
+            <loc>{{ str_replace(config('url_mappings.source_domain'), parse_url(env('APP_URL'), PHP_URL_HOST), $url->url) }}</loc>
             @if($url->last_modified)
                 <lastmod>{{ \Carbon\Carbon::parse($url->last_modified)
                 ->setTimezone('Asia/Ho_Chi_Minh')
