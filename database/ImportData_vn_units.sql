6aa4fc4b0a57d6c1fa2cf7b1567a2b0228768aa1
dcstylexf <dcstylexf@gmail.com>
2025-04-16 20:39:43 +0700

    Update

--- database/ImportData_vn_units.sql
+++ database/ImportData_vn_units.sql
@@ -3,6 +3,30 @@
 /* Reference: https://github.com/ThangLeQuoc/vietnamese-provinces-database */
 /* =============================================== */
 
+-- DATA for administrative_regions --
+INSERT INTO administrative_regions(id,name,name_en,code_name,code_name_en) VALUES(1,'Đông Bắc Bộ','Northeast','dong_bac_bo','northest');
+INSERT INTO administrative_regions(id,name,name_en,code_name,code_name_en) VALUES(2,'Tây Bắc Bộ','Northwest','tay_bac_bo','northwest');
+INSERT INTO administrative_regions(id,name,name_en,code_name,code_name_en) VALUES(3,'Đồng bằng sông Hồng','Red River Delta','dong_bang_song_hong','red_river_delta');
+INSERT INTO administrative_regions(id,name,name_en,code_name,code_name_en) VALUES(4,'Bắc Trung Bộ','North Central Coast','bac_trung_bo','north_central_coast');
+INSERT INTO administrative_regions(id,name,name_en,code_name,code_name_en) VALUES(5,'Duyên hải Nam Trung Bộ','South Central Coast','duyen_hai_nam_trung_bo','south_central_coast');
+INSERT INTO administrative_regions(id,name,name_en,code_name,code_name_en) VALUES(6,'Tây Nguyên','Central Highlands','tay_nguyen','central_highlands');
+INSERT INTO administrative_regions(id,name,name_en,code_name,code_name_en) VALUES(7,'Đông Nam Bộ','Southeast','dong_nam_bo','southeast');
+INSERT INTO administrative_regions(id,name,name_en,code_name,code_name_en) VALUES(8,'Đồng bằng sông Cửu Long','Mekong River Delta','dong_bang_song_cuu_long','southwest');
+------------------------------------
+
+-- DATA for administrative_units --
+INSERT INTO administrative_units(id,full_name,full_name_en,short_name,short_name_en,code_name,code_name_en) VALUES(1,'Thành phố trực thuộc trung ương','Municipality','Thành phố','City','thanh_pho_truc_thuoc_trung_uong','municipality');
+INSERT INTO administrative_units(id,full_name,full_name_en,short_name,short_name_en,code_name,code_name_en) VALUES(2,'Tỉnh','Province','Tỉnh','Province','tinh','province');
+INSERT INTO administrative_units(id,full_name,full_name_en,short_name,short_name_en,code_name,code_name_en) VALUES(3,'Thành phố thuộc thành phố trực thuộc trung ương','Municipal city','Thành phố','City','thanh_pho_thuoc_thanh_pho_truc_thuoc_trung_uong','municipal_city');
+INSERT INTO administrative_units(id,full_name,full_name_en,short_name,short_name_en,code_name,code_name_en) VALUES(4,'Thành phố thuộc tỉnh','Provincial city','Thành phố','City','thanh_pho_thuoc_tinh','provincial_city');
+INSERT INTO administrative_units(id,full_name,full_name_en,short_name,short_name_en,code_name,code_name_en) VALUES(5,'Quận','Urban district','Quận','District','quan','urban_district');
+INSERT INTO administrative_units(id,full_name,full_name_en,short_name,short_name_en,code_name,code_name_en) VALUES(6,'Thị xã','District-level town','Thị xã','Town','thi_xa','district_level_town');
+INSERT INTO administrative_units(id,full_name,full_name_en,short_name,short_name_en,code_name,code_name_en) VALUES(7,'Huyện','District','Huyện','District','huyen','district');
+INSERT INTO administrative_units(id,full_name,full_name_en,short_name,short_name_en,code_name,code_name_en) VALUES(8,'Phường','Ward','Phường','Ward','phuong','ward');
+INSERT INTO administrative_units(id,full_name,full_name_en,short_name,short_name_en,code_name,code_name_en) VALUES(9,'Thị trấn','Commune-level town','Thị trấn','Township','thi_tran','commune_level_town');
+INSERT INTO administrative_units(id,full_name,full_name_en,short_name,short_name_en,code_name,code_name_en) VALUES(10,'Xã','Commune','Xã','Commune','xa','commune');
+------------------------------------
+
 -- DATA for provinces --
 INSERT INTO provinces(code,name,name_en,full_name,full_name_en,code_name,administrative_unit_id,administrative_region_id) VALUES
 ('01','Hà Nội','Ha Noi','Thành phố Hà Nội','Ha Noi City','ha_noi',1,3),
